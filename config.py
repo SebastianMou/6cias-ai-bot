a26a22d2ad2c25ae668127d248993f0d1726e3e1
SebastianMou <sebastian_mou@protonmail.com>
2026-08-05 13:24:31 -0600

    new domain

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from typing import List
 class Settings(BaseSettings):
     gemini_api_key: str
     database_url: str = "sqlite:///./chatbot.db"
-    allowed_origins: str = "https://6cias.com,http://localhost"
+    allowed_origins: str = "https://www.6cias.com,http://localhost"
     cloudinary_cloud_name: str = "dzinwey2x"
     cloudinary_api_key: str = "397936317813431"
     cloudinary_api_secret: str = "iEkqCLJ5hzTsggGKNGRG4JxxdSw"
